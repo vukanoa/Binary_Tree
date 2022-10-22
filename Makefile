a3da5bdba60efe5e75292a76be525db8171e6abb
Noa Vukasovic <noavukasovic@gmail.com>
2022-10-22 20:34:19 +0200

    Improved Makefile & Deployed *.c & *.h files in their respective directories & Removed commented explanation on visual_print in tree.h

--- Makefile
+++ Makefile
@@ -1,9 +1,13 @@
 CC	:= gcc
 CLFAGS	:= -O0 -g -Wall
 TARGET	:= program
-HDRS	:= -I.
-SRCS	:= $(wildcard *.c)
-OBJS	:= $(patsubst %.c,%.o,$(SRCS))
+BUILD	:= build
+SRCDIR	:= src
+HDRDIR	:= hdr
+HDRS	:= -I$(HDRDIR)
+SRCS	:= $(wildcard $(SRCDIR)/*.c)
+OBJS	:= $(patsubst $(SRCDIR)/%.c,$(BUILD)/%.o,$(SRCS))
+DEPS	:= $(patsubst $(SRCDIR)/%.c,$(BUILD)/%.d,$(SRCS))
 
 .PHONY = all
 
@@ -12,10 +16,16 @@ all: $(TARGET)
 $(TARGET): $(OBJS)
 	$(CC) $(CFLAGS) $^ -o $@
 
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
+$(BUILD)/%.o: $(SRCDIR)/%.c | $(BUILD)
+	$(CC) $(CFLAGS) $(HDRS) -c $< -o $@
+
+$(BUILD)/%.d: $(SRCDIR)/%.c | $(BUILD)
+	$(CC) $(HDRS) -MM -MT $(BUILD)/$*.o $< > $@
+
+include $(DEPS)
+
+$(BUILD):
+	mkdir build
 
 clean:
-	$(RM) \
-	$(TARGET) \
-	$(OBJS)
+	rm -rf $(TARGET) $(BUILD)
---
+++ hdr/tree.h
@@ -0,0 +1,39 @@
+#ifndef TREE_H_
+#define TREE_H_
+
+#include <stdio.h>
+
+struct Node{
+	int data;
+	struct Node* left;
+	struct Node* right;
+	int level;
+};
+
+// Binary Tree functions
+void insert	(struct Node** root, int data);
+void find	(struct Node*  root, int data);
+
+/* Orders of printing */
+
+// Recursive priting
+void print_preorder (struct Node* root);
+void print_inorder  (struct Node* root);
+void print_postorder(struct Node* root);
+
+// Iterative printing
+void iter_print_preorder  (struct Node* root);
+void iter_print_inorder   (struct Node* root);
+void iter_print_postorder (struct Node* root);
+void iter_print_levelorder(struct Node* root);
+
+// Visual display of a Binary Tree
+void visual_print(struct Node* head);
+
+// Util functions
+int int_pow(int base, int exp);
+int calculate_spaces(int levels, int head_level);
+int check_power_of_two(int number);
+int which_power_of_two(int two_pow);
+
+#endif //TREE_H
